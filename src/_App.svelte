a45c3518c9128e0cf2466ccea34ec5e746aaee7d
Andrew Wong <andrew.j.wong@outlook.com>
2019-12-31 15:32:59 +1100

    Add async proxy

---
+++ src/_App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import PouchDB from "pouchdb-browser";
+
+  async function load() {
+    let database = {
+      items: new PouchDB("items"),
+      categories: new PouchDB("categories")
+    };
+
+    let items = await database.items.allDocs({ include_docs: true });
+    let categories = await database.items.allDocs({ include_docs: true });
+    return { database, items, categories };
+  }
+
+  import App from "./App.svelte";
+</script>
+
+{#await load() then data}
+  <App {data} />
+{/await}
