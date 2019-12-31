6348253f6f811ebefb0bbe3bab16183a8d42f8e2
Andrew Wong <andrew.j.wong@outlook.com>
2019-12-31 11:33:45 +1100

    Add base Carousel elements

---
+++ src/components/Carousel/Carousel.svelte
@@ -0,0 +1,17 @@
+<script>
+  import CarouselEntry from "./Entry.svelte";
+</script>
+
+<style>
+  .carousel {
+    height: 100px;
+    width: 100px;
+    border: 3px solid black;
+    border-radius: 20px;
+    margin: 0 auto;
+  }
+</style>
+
+<div class="carousel">
+  <CarouselEntry />
+</div>
