6da7dbb6250dc51fbb7c44c21420394fb10b362a
Andrew Wong <andrew.j.wong@outlook.com>
2019-12-31 11:33:57 +1100

    Add container for Carousel

---
+++ src/components/Carousel/Container.svelte
@@ -0,0 +1,22 @@
+<script>
+  let selected;
+  export let data;
+
+  import Carousel from "./Carousel.svelte";
+  import CarouselMenu from "./Menu.svelte";
+
+  function left() {
+    console.log("leff");
+  }
+</script>
+
+<style>
+  .container > :global(*) {
+    display: inline-block;
+  }
+</style>
+
+<div class="container">
+  <Carousel />
+  <CarouselMenu on:random on:left on:right />
+</div>
